51f81cacca2409927481dd9a61ee9f7fce4eca5b
Joczik Szabolcs <joczikszabi@gmail.com>
2021-12-02 05:12:53 +0100

    feat(Updated algorithm)
    Updated overall project structure and implemented latest version of the distance estimator algorithm.
    Use distance_test.py for testing purposes

--- main/DistanceEstimation.py
+++ main/DistanceEstimation.py
@@ -13,116 +13,71 @@ class DistanceEstimation:
         configParser = configparser.ConfigParser()
         configFilePath = os.path.join(os.path.dirname(__file__), '..', 'config', 'config.txt')
         configParser.read(configFilePath)
-        
-        #self.focal_length = configParser.getfloat('CONFIG', 'CAMERA_FOCAL_LENGTH')
-        #self.object_real_width = configParser.getfloat('CONFIG', 'OBJECT_REAL_WIDTH')
 
         self.distance_between_nodes = configParser.getfloat('CONFIG', 'DISTANCE_BETWEEN_NODES')
+        self.grid_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'grid.json')
 
         if generateDummy:
-            self.grid = self.generateDummyGrid()
+            self.generateDummyGrid()
         else:
-            grid_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'grid.json')
-            with open(grid_path, "r") as f:
-                # IMPORTANT!!!!
-                # SORT coordinates based on x and y coordinates and save them in a 2d array
-                gridjson = json.load(f)
-                self.grid = []
-                for row in gridjson["nodes"]:
-                    new_row = []
+            self.loadGridFromJson()
 
-                    for node in row:
-                        new_row.append(tuple(node))
-                    self.grid.append(new_row)
+            
+    def loadGridFromJson(self):
+        # Loads grid layout from json data file
+        #
+        # Returns: N x M numpy array containing the grid coordinates
 
-                self.grid = np.array(self.grid)
+        with open(self.grid_path, "r") as f:
+            self.grid_json = json.load(f)
+            grid = []
+
+            for row in self.grid_json["nodes"]:
+                new_row = []
+                for node in row:
+                    new_row.append(tuple(node))
+                grid.append(new_row)
+
+            self.grid = np.array(grid, dtype=object)
+
 
     def generateDummyGrid(self):
         xvalues = np.linspace(0.0, 1920.0, num=20)
         yvalues = np.linspace(0.0, 1080.0, num=10)
 
         xx, yy = np.meshgrid(xvalues, yvalues)
-        coords = [(a1, b1,) for a, b in zip(xx, yy) for a1, b1 in zip(a, b)]
+        coords = [(a1, b1) for a, b in zip(xx, yy) for a1, b1 in zip(a, b)]
 
-        return coords
+        self.grid = coords
 
 
     def getClosestNode(self, coordinate):
-    # Finds closest node in the grid to the coordinate(x, y)
-    #
-    # Input parameters:
-    # coordinate(x, y): Touple where x and y coordinates are in pixels
-    #
-    # Returns:
-    # Closest node to the given coordinate
-        print(self.grid.flatten())
-        return spatial.KDTree(self.grid.flatten()).query(coordinate)
-
-    def findNextNodeVertical(self, node):
-        minY = 10000
-        nextNode = []
-
-        for grid_point in self.grid:
-            diffY = node[1] - grid_point[1]
-            if abs(grid_point[0] - node[0 ]) < 60 and diffY > 0 and diffY < minY:
-                nextNode = grid_point
-                minY = diffY
+        # Finds closest node in the grid to the coordinate(x, y)
+        #
+        # Input parameters:
+        # coordinate(x, y): Touple where x and y coordinates are in pixels
+        #
+        # Returns:
+        # Closest node to the given coordinate
 
 
-        print(f"NextNode vertically: {nextNode}")
-        return nextNode
+        grid_flatten = [(node[0][0], node[0][1]) if node[0] != () else (9999, 9999) for node in self.grid.reshape(120,1)]
+        print(grid_flatten)
+        dist_ind = spatial.KDTree(grid_flatten).query(coordinate)
 
-    def findNextNodeHorizontal(self, node):
-        minX = 10000
-        nextNode = []
-
-        for grid_point in self.grid:
-            diffX = grid_point[0] - node[0]
-            if abs(grid_point[1] - node[1]) < 40 and diffX > 0 and diffX < minX:
-                nextNode = grid_point
-                minX = diffX
-
-        print(f"NextNode horizontally: {nextNode}")
-        return nextNode
-
-
-    def findNumberOfNodesBetween(self, node1, node2):
-        numNodesX = 0
-        nextNodeX = node1
-        
-        while True:
-            nextNodeX = self.findNextNodeHorizontal(nextNodeX)
-
-            if nextNodeX and nextNodeX[0] <= node2[0]:
-                numNodesX += 1
-            else:
-                break
-
-        numNodesY = 0
-        nextNodeY = node1
-        while True:
-            nextNodeY = self.findNextNodeVertical(nextNodeY)
-
-            if nextNodeY and node2[1] <= nextNodeY[1]:
-                numNodesY += 1
-            else:
-                print("alma?")
-                break
-
-        print(f"xadj: {numNodesX} yadj: {numNodesY}")
-
-        return [numNodesX, numNodesY]
+        # Convert back index to 2d array index format
+        ind = (math.floor(dist_ind[1] / self.grid.shape[1]), dist_ind[1] % self.grid.shape[1])
+        print((dist_ind[0], ind))
+        return (dist_ind[0], ind)
     
     def estimateDistance(self, coordinate1, coordinate2):
         dist1, node_ind1 = self.getClosestNode(coordinate1)
         dist2, node_ind2 = self.getClosestNode(coordinate2)
-        print(f"closest to ball: ({self.grid[node_ind1]})")
-        print(f"closest to hole: ({self.grid[node_ind2]})")
+        print(f"closest to ball: {self.grid[node_ind1[0], node_ind1[1]]}")
+        print(f"closest to hole: {self.grid[node_ind2[0], node_ind2[1]]}")
 
-        numNodesBetween = self.findNumberOfNodesBetween(self.grid[node_ind1], self.grid[node_ind2])
-
-        a = numNodesBetween[0] * self.distance_between_nodes
-        b = numNodesBetween[1] * self.distance_between_nodes
+        a = abs(node_ind1[0] - node_ind2[0]) * self.distance_between_nodes
+        b = abs(node_ind1[1] - node_ind2[1]) * self.distance_between_nodes
 
         dist = math.sqrt(a**2 + b**2)
 
