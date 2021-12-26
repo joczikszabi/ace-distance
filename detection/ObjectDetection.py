6e7b6a20a6e034a5ff82aaf80eae4a4a3b189caf
Joczik Szabolcs <joczikszabi@gmail.com>
2021-12-26 11:18:17 +0100

    Substract before-after contours

--- detection/ObjectDetection.py
+++ detection/ObjectDetection.py
@@ -13,107 +13,12 @@ class ObjectDetection:
 
         self.img_path = img_path
         self.img = cv2.imread(img_path)
-        self.img_name,_ = os.path.splitext(os.path.basename(img_path))
+        self.img_name,_ = os.path.splitext(os.path.basename(img_path))  
 
 
-    def draw_lines(self, hough, image, nlines):
-        n_x, n_y=image.shape
-        print(hough[0])
-        #convert to color image so that you can see the lines
-        draw_im = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-
-        for (rho, theta) in hough[0][:nlines]:
-            print(f"rho,theta({rho}, {theta})")
-            try:
-                x0 = np.cos(theta)*rho
-                y0 = np.sin(theta)*rho
-                pt1 = ( int(x0 + (n_x+n_y)*(-np.sin(theta))), int(y0 + (n_x+n_y)*np.cos(theta)) )
-                pt2 = ( int(x0 - (n_x+n_y)*(-np.sin(theta))), int(y0 - (n_x+n_y)*np.cos(theta)) )
-                alph = np.arctan( (pt2[1]-pt1[1])/( pt2[0]-pt1[0]) )
-                alphdeg = alph * 180 / np.pi
-
-                print(f"({pt1}, {pt2})")
-
-                #OpenCv uses weird angle system, see: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-                if abs( np.cos( alph - 180 )) > 0.8: #0.995:
-                    cv2.line(draw_im, pt1, pt2, (255,0,0), 2)
-                if rho > 0 and abs( np.cos( alphdeg - 90)) > 0.7:
-                    cv2.line(draw_im, pt1, pt2, (0,0,255), 2)    
-            except:
-                pass
-
-        cv2.imwrite("3HoughLines.png", draw_im, [cv2.IMWRITE_PNG_COMPRESSION, 12])   
-
-            
-    def findAceHole(self):
-        image = self.img.copy()
-        image = image[500:self.img.shape[0]-100, 300:self.img.shape[1]-300]
-        cv2.imwrite("image.jpg", image)
-        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("0gray.jpg", gray)
-
-        element = np.ones((6,6))
-        erode = cv2.erode(gray, element)
-        cv2.imwrite("1erodedtresh.jpg", erode)
-
-        # adaptive threshold
-        #thresh = cv2.adaptiveThreshold(erode,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,51,9)
-        #cv2.imwrite("2thresh.jpg", thresh)
-
-        # Fill rectangular contours
-        #cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #vcnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        #for c in cnts:
-        #    cv2.drawContours(thresh, [c], -1, (255,255,255), -1)
-
-        # Morph open
-        #kernel = np.ones((6,2),np.uint8)
-        #opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=4)
-        #cv2.imwrite("3mop.jpg", opening)
-
-
-        # blur
-        blur = cv2.GaussianBlur(erode, (0,0), sigmaX=125, sigmaY=125)
-        cv2.imwrite("2blur.jpg", blur)
-
-        # divide
-        divide = cv2.divide(gray, blur, scale=255)
-        cv2.imwrite("3divide.jpg", divide)
-
-        # otsu threshold
-        thresh = cv2.threshold(divide, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-        cv2.imwrite("4tresh.jpg", thresh)
-
-        # apply morphology
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,4))
-        morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-        cv2.imwrite("5morph.jpg", morph)
-
-        # Draw rectangles, the 'area_treshold' value was determined empirically
-        cnts = cv2.findContours(morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        area_treshold = 4000
-        for c in cnts:
-            if cv2.contourArea(c) > area_treshold :
-              x,y,w,h = cv2.boundingRect(c)
-              cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
-
-        cv2.imwrite("result.jpg", image)
-
-
-        minLineLength = 50
-        maxLineGap = 20
-        #hough = cv2.HoughLines(thresh, 1, np.pi / 180, 50, None, 50, 10) 
-        #print(hough)
-        #self.draw_lines(hough, erode, 10) 
-
-    def findAceHole2(self):
-        # Create directory for outputs
-        out_dir = f"./tmp/{self.img_name}"
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-
-        out_dir = f"./tmp/{self.img_name}/hole"
+    def findAceHole2(self, out_dir):
+        # Create directory for outputss
+        out_dir = f"{out_dir}/hole"
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
@@ -147,12 +52,6 @@ class ObjectDetection:
 
         cv2.imwrite(f"{out_dir}/5contour.jpg", opening)
 
-
-
-        #Crop bottom
-        #cropped = dilate[50:dilate.shape[0]-300, 0:dilate.shape[1]]
-        #cv2.imwrite("./tmp/6cropped.jpg", cropped)
-
         contours, hierarchy = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         selected_contour = None
         max_area = 0
@@ -179,19 +78,14 @@ class ObjectDetection:
         y = max(cnt2[:, 1])
 
         # Draw the contour on the new mask and perform the bitwise operation
-        #res = cv2.drawContours(image, [cnt2],-1, 255, -1)
         res = cv2.circle(image_orig, (x,y-5), 2, (0, 0, 255), 2)
         cv2.imwrite(f"{out_dir}/6result.jpg", res)
-        return (x,y-5)
+        return (x,y-10)
 
 
-    def findGolfBall(self):
+    def findGolfBall(self, img_before_path, out_dir):
         # Create directory for outputs
-        out_dir = f"./tmp/{self.img_name}"
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-
-        out_dir = f"./tmp/{self.img_name}/ball"
+        out_dir = f'{out_dir}/ball'
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
@@ -199,28 +93,27 @@ class ObjectDetection:
         image = image_orig[500:self.img.shape[0]-100, 300:self.img.shape[1]-300]
         cv2.imwrite(f"{out_dir}/0image.jpg", image)
 
-        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(f"{out_dir}/1gray.jpg", gray)
+        image_before = cv2.imread(img_before_path)
+        image_before = image_before[500:image_before.shape[0]-100, 300:image_before.shape[1]-300]
 
-        #whiten = cv2.convertScaleAbs(gray, alpha=1.15, beta=1.5)
-        #cv2.imwrite(f"{out_dir}/2whiten.jpg", whiten)
 
-        _, thresh = cv2.threshold(gray, 230, 255, cv2.THRESH_BINARY)
-        cv2.imwrite(f"{out_dir}/3tresh.jpg", thresh)
+        # Get contours and subtract the contours from the before image
+        contours_before = self.getContoursForBall(image_before, out_dir)
+        cv2.imwrite(f"{out_dir}/3a_contours_before.jpg", contours_before)
 
-        # Remove small noise by filtering using contour area
-        cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        contours_after  = self.getContoursForBall(image, out_dir)
+        cv2.imwrite(f"{out_dir}/3b_contours_after.jpg", contours_after)
 
-        if not cnts:
-            _, thresh = cv2.threshold(gray, 190, 255, cv2.THRESH_BINARY)
-            cv2.imwrite(f"{out_dir}/3btresh.jpg", thresh)
+        contours = cv2.subtract(contours_after, contours_before)
+        cv2.imwrite(f"{out_dir}/3c_contours_substract.jpg", contours)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
-        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations = 1)
+
+        # Apply morphology for cleaning up noise
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,1))
+        opening = cv2.morphologyEx(contours, cv2.MORPH_OPEN, kernel, iterations = 1)
         cv2.imwrite(f"{out_dir}/4morph.jpg", opening)
 
-        # Remove small noise by filtering using contour area
+        # Remove small noise by removing contours with too large or small area
         cnts = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
@@ -233,7 +126,6 @@ class ObjectDetection:
             y_min = np.min(contour_np[:, 1])
             y_max = np.max(contour_np[:, 1])
             y_avg = np.mean(contour_np[:, 1])
-            print(f"area:{cv2.contourArea(c)}")
 
             if cv2.contourArea(c) > 25 or abs(y_min - y_max) > 6 or abs(x_min - x_max) > 6 or x_avg < 100:
                 cv2.drawContours(opening,[c], 0, (0,0,0), -1)
@@ -280,3 +172,21 @@ class ObjectDetection:
             return (x,y)
 
         return None
+
+
+    def getContoursForBall(self, image, out_dir):
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(f"{out_dir}/1gray.jpg", gray)
+
+        _, thresh = cv2.threshold(gray, 230, 255, cv2.THRESH_BINARY)
+        cv2.imwrite(f"{out_dir}/2tresh.jpg", thresh)
+
+        # Remove small noise by filtering using contour area
+        cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+        if not cnts:
+            _, thresh = cv2.threshold(gray, 190, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(f"{out_dir}/2btresh.jpg", thresh)
+
+        return thresh
--- object_test.py
+++ object_test.py
@@ -6,38 +6,38 @@ from detection.ObjectDetection import ObjectDetection
 from main.DistanceEstimation import DistanceEstimation
 
 
-def runTest(img_path):
-	img = cv2.imread(img_path)
-	img_orig = img.copy()
-	img_name,_ = os.path.splitext(os.path.basename(img_path))
+def runTest(img_name):
+	img_before_path = f"{img_name}-before.png"
+	img_after_path 	= f"{img_name}-after.png"
+
+	img_before = cv2.imread(img_before_path)
+	img_after = cv2.imread(img_after_path)
+	img_after_orig = img_after.copy()
 
 	# Create directory for outputs
 	out_dir = f"./tmp/{img_name}"
 	if not os.path.exists(out_dir):
 		os.makedirs(out_dir)
 
-	out_dir = f"./tmp/{img_name}/results"
-	if not os.path.exists(out_dir):
-		os.makedirs(out_dir)
-
 	# Save image and grid layout
-	cv2.imwrite(f"{out_dir}/0image_orig.jpg", img_orig)
+	cv2.imwrite(f"{out_dir}/0image_orig_before.jpg", img_before)
+	cv2.imwrite(f"{out_dir}/0image_orig_after.jpg", img_after_orig)
 
-	img_grid = plot_grid(img_orig, estimator.grid)
-	cv2.imwrite(f"{out_dir}/1image_grid.jpg", img_grid)
+	img_grid = plot_grid(img_after_orig, estimator.grid)
+	cv2.imwrite(f"{out_dir}/1image_grid.jpg", img_after_orig)
 
 	# Detect license plate on image provided as a command line argument
 	print(f"\nCurrent image: {img_name}")
-	det = ObjectDetection(f"./{img_path}")
-	pos_hole = det.findAceHole2()
-	pos_ball = det.findGolfBall()
+	det = ObjectDetection(f"./{img_after_path}")
+	pos_hole = det.findAceHole2(out_dir)
+	pos_ball = det.findGolfBall(img_before_path, out_dir)
 
 	if pos_hole is not None and pos_ball is not None:
 		# displaying the coordinates on the image window
 		font = cv2.FONT_HERSHEY_SIMPLEX
-		cv2.circle(img, pos_hole, 5, (0, 255, 255), -1)
-		cv2.circle(img, pos_ball, 5, (255, 0, 0), -1)
-		cv2.line(img, pos_ball, pos_hole, (255, 0, 0), 1)
+		cv2.circle(img_after, pos_hole, 5, (0, 255, 255), -1)
+		cv2.circle(img_after, pos_ball, 5, (255, 0, 0), -1)
+		cv2.line(img_after, pos_ball, pos_hole, (255, 0, 0), 1)
 
 		# Do distance estimation
 		dist = estimator.estimateDistance(pos_ball, pos_hole)
@@ -49,16 +49,16 @@ def runTest(img_path):
 			print(f"Estimated distance between ball and hole: {dist} meter(s)\n")
 
 			# displaying the coordinates on the image window
-			cv2.putText(img, f"{str(dist)}m", (int((pos_ball[0]+pos_hole[0])/2)-75, int((pos_ball[1]+pos_hole[1])/2-25)), font, 1, (255, 0, 0), 2)
+			cv2.putText(img_after, f"{str(dist)}m", (int((pos_ball[0]+pos_hole[0])/2)-75, int((pos_ball[1]+pos_hole[1])/2-25)), font, 1, (255, 0, 0), 2)
 		else:
-			cv2.putText(img, f"Out of grid", (int((pos_ball[0]+pos_hole[0])/2)-125, int((pos_ball[1]+pos_hole[1])/2-25)), font, 1, (255, 0, 0), 2)
+			cv2.putText(img_after, f"Out of grid", (int((pos_ball[0]+pos_hole[0])/2)-125, int((pos_ball[1]+pos_hole[1])/2-25)), font, 1, (255, 0, 0), 2)
 		
-		cv2.imwrite(f"{out_dir}/distance_estimation.jpg", img)
+		cv2.imwrite(f"{out_dir}/distance_estimation.jpg", img_after)
 		print(f"Image saved at: {out_dir}/3distance_estimation.jpg\n")
 
 # Cases
 estimator = DistanceEstimation()
 
-imgs = ["699-after.png", "708-after.png", "715-after.png", "753-before.png", "763-after.png", "793-Before.png"]
+imgs = ["699", "708", "715", "753", "763", "768", "770"]
 for img in imgs:
 	runTest(img)
